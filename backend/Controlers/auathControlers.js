786f232d623a0f50a410f27616a42861aad81a94
Ujjwal kumar <uk7320942276@gmail.com>
2026-01-29 19:14:26 +0530

    Update auathControlers.js

--- backend/Controlers/auathControlers.js
+++ backend/Controlers/auathControlers.js
@@ -162,6 +162,7 @@ export const forgotPasswordOtp = async (req, res) => {
 
     res.status(200).json({ message: "OTP sent to email." });
   } catch (error) {
+    console.log(error)
     res.status(500).json({ message: error.message });
   }
 };
