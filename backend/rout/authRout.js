fef53b4225f7afa94686c9b7da011180f18aa9db
Ujjwal kumar <uk7320942276@gmail.com>
2026-01-31 11:23:34 +0530

    Update authRout.js

--- backend/rout/authRout.js
+++ backend/rout/authRout.js
@@ -10,13 +10,14 @@ import {
   deleteUser,
   simpleSignUp,
 } from "../Controlers/auathControlers.js";
+import islogin from "../Midlewhere/isLogin.js";
 
 const router = express.Router();
 
 router.post("/signup-otp", signUpOtp);
 router.post("/verify-signup-otp", verifyOtpSignup);
 router.post("/signin", signIn);
-router.get("/signout", signOut);
+router.get("/signout", islogin , signOut);
 router.post("/forgot-password-otp", forgotPasswordOtp);
 router.post("/verify-forgot-password-otp", verifyForgotOtp);
 router.post("/reset-password", resetPassword);
